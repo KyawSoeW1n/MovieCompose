bb5da41a23f929196ccf352fe50e5615ec0a5cd7
kyawsoewin <kyawsoewin.august@gmail.com>
2023-12-18 16:16:57 +0630

    Implement baseline profile

--- app/benchmark/build.gradle.kts
+++ app/benchmark/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.kurio.tetsuya.movie.benchmark"
-    compileSdk = 33
+    compileSdk = libs.versions.compileSdk.get().toInt()
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
@@ -19,7 +19,7 @@ android {
 
     defaultConfig {
         minSdk = 24
-        targetSdk = 33
+        targetSdk = libs.versions.targetSdk.get().toInt()
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- app/benchmark/src/main/java/com/kurio/tetsuya/movie/benchmark/BaselineProfileGenerator.kt
+++ app/benchmark/src/main/java/com/kurio/tetsuya/movie/benchmark/BaselineProfileGenerator.kt
@@ -2,14 +2,12 @@ package com.kurio.tetsuya.movie.benchmark
 
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.benchmark.macro.ExperimentalBaselineProfilesApi
 import androidx.benchmark.macro.junit4.BaselineProfileRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-@OptIn(ExperimentalBaselineProfilesApi::class)
 @RunWith(AndroidJUnit4::class)
 class BaselineProfileGenerator {
 
@@ -19,7 +17,7 @@ class BaselineProfileGenerator {
 
     @RequiresApi(Build.VERSION_CODES.P)
     @Test
-    fun generateBaselineProfile() = baselineRule.collectBaselineProfile(
+    fun generateBaselineProfile() = baselineRule.collect(
         packageName = "com.kurio.tetsuya.movie.compose.dev",
     ) {
         pressHome()
